db4a1add083922f1330eef40712a98807cbea715
Sebastian Walter <sebwalter@gmx.net>
2018-01-19 08:19:28 +0100

    updated nuget packages to their latest version

--- EvilBaschdi.Core/Properties/AssemblyInfo.cs
+++ EvilBaschdi.Core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EvilBaschdi.Core")]
-[assembly: AssemblyCopyright("Copyright © evilbaschdi 2015 - 2017")]
+[assembly: AssemblyCopyright("Copyright © evilbaschdi 2015 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
