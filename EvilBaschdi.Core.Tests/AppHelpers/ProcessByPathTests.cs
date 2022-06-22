74dddd1405b0c08ad62a8503d6facc2c1b1e3149
Sebastian Walter <sebwalter@gmx.net>
2022-06-22 18:28:47 +0200

    Moved from EvilBaschdi.CoreExtended

---
+++ EvilBaschdi.Core.Tests/AppHelpers/ProcessByPathTests.cs
@@ -0,0 +1,25 @@
+﻿using EvilBaschdi.Core.AppHelpers;
+
+namespace EvilBaschdi.Core.Tests.AppHelpers
+{
+    public class ProcessByPathTests
+    {
+        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+        public void Constructor_HasNullGuards(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(ProcessByPath).GetConstructors());
+        }
+
+        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+        public void Constructor_ReturnsInterfaceName(ProcessByPath sut)
+        {
+            sut.Should().BeAssignableTo<IProcessByPath>();
+        }
+
+        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+        public void Methods_HaveNullGuards(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(ProcessByPath).GetMethods().Where(method => !method.IsAbstract));
+        }
+    }
+}
