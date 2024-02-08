d220f24e6f846cb085b317bd189dc60efcf32196
Sebastian Walter <sebwalter@gmx.net>
2024-02-08 18:04:29 +0100

    cleanup

--- EvilBaschdi.Core.Tests/Internal/FileListFromPathTests.cs
+++ EvilBaschdi.Core.Tests/Internal/FileListFromPathTests.cs
@@ -1,5 +1,4 @@
 using EvilBaschdi.Core.Internal;
-using EvilBaschdi.Core.Model;
 
 namespace EvilBaschdi.Core.Tests.Internal;
 
@@ -23,24 +22,24 @@ public class FileListFromPathTests
     //    assertion.Verify(typeof(FileListFromPath).GetMethods().Where(method => !method.IsAbstract));
     //}
 
-/*
-    [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
-    public void ValueFor_WithFilters_Result(
-        FileListFromPath sut)
-    {
-        // Arrange
-        var filePathFilter = new FileListFromPathFilter
-                             {
-                                 FilterExtensionsToEqual = ["txt"]
-                             };
+    /*
+        [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
+        public void ValueFor_WithFilters_Result(
+            FileListFromPath sut)
+        {
+            // Arrange
+            var filePathFilter = new FileListFromPathFilter
+                                 {
+                                     FilterExtensionsToEqual = ["txt"]
+                                 };
 
-        // Act
-        var result = sut.ValueFor(@"C:\temp", filePathFilter);
+            // Act
+            var result = sut.ValueFor(@"C:\temp", filePathFilter);
 
-        // Assert
-        result.Should().HaveCount(5);
-    }
-    */
+            // Assert
+            result.Should().HaveCount(5);
+        }
+        */
 
     [Theory, NSubstituteOmitAutoPropertiesTrueAutoData]
     public void GetSubdirectoriesContainingOnlyFiles_WithoutFilter_Result(
